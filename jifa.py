1e8643c767b732c4a1a78c5d7e48bf3fcc251102
PacinoMeng <2387057769@qq.com>
2024-08-07 22:52:10 +0800

    Add files via upload

---
+++ jifa.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+'''
+作者：Edison
+日期：2024-08-07
+'''
+
+print(input('请输入任意按键：'))
+for i in range(1):
+    for j in range(1,53):
+        print(' ',end='')
+    for j in range(1,7):
+        print('*',end='')
+    print()
+for i in range(1):
+    for j in range(1,52):
+        print(' ',end='')
+    for j in range(1,9):
+        print('*',end='')
+    print()
+for i in range(1):
+    for j in range(1,51):
+        print(' ',end='')
+    for j in range(1,11):
+        print('*',end='')
+    print()
+for i in range(1,5):
+    for j in range(1,48): #48=51-3
+        print(' ',end='')
+    for j in range(1,17): #17=11+3*2
+        print('*',end='')
+    print()
+for i in range(1,12):
+    for j in range(1,51):
+        print(' ',end='')
+    for j in range(1,11):
+        print('*',end='')
+    print()
+for i in range(1):
+    for j in range(1,38):
+        print(' ',end='')
+    for j in range(1,17):
+        print('*',end='')
+    for j in range(1,4):
+        print(' ',end='')
+    for j in range(1,17):
+            print('*', end='')
+    print()
+for i in range(1,5):
+    for j in range(1,37):
+        print(' ',end='')
+    for j in range(1,19):
+        print('*',end='')
+    for j in range(1,2):
+        print(' ',end='')
+    for j in range(1,19):
+            print('*', end='')
+    print()
+for i in range(1):
+    for j in range(1,38):
+        print(' ',end='')
+    for j in range(1,17):
+        print('*',end='')
+    for j in range(1,4):
+        print(' ',end='')
+    for j in range(1,17):
+            print('*', end='')
+    print()
